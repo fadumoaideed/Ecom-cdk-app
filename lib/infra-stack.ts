5e460a1c98a32c7bd834822216e5c4c0a2d7afe0
fadumoaideed <fadumo.ahmed.aideed@gmail.com>
2023-02-16 13:33:37 +0000

    CDK implemented with ec2 instance to run the app

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,16 +1,82 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { Vpc, Instance, IpAddresses } from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as keypair from "cdk-ec2-key-pair"; // Helper to create EC2 SSH keypairs
 
-export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface Env {
+  account: string;
+  region: string;
+}
+
+interface InfraStackProps extends StackProps {
+  env: Env;
+}
+
+export class InfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: InfraStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, "economist-navbar-vpc", {
+      ipAddresses: IpAddresses.cidr("10.0.0.0/16"),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const securityGroup = new ec2.SecurityGroup(
+      this,
+      "economist-navbar-security-group",
+      {
+        vpc: vpc,
+      }
+    );
+
+    const key = new keypair.KeyPair(this, "KeyPair", {
+      name: "cdk-keypair",
+      description: "Key Pair created with CDK Deployment",
+    });
+    key.grantReadOnPublicKey;
+
+    // Allow HTTP access on port tcp/80
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      "Access to port 80"
+    );
+
+    // Allow SSH access on port tcp/22
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "Allow SSH Access"
+    );
+    // IAM role to allow access to other AWS services
+    const role = new iam.Role(this, "ec2Role", {
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+    });
+
+    // IAM policy attachment to allow access to
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    );
+
+    new Instance(this, "Instance", {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroup: securityGroup,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        cpuType: ec2.AmazonLinuxCpuType.X86_64,
+      }),
+      keyName: key.keyPairName,
+    });
+
+    // Define ports to allow traffic into vpc instance
+    // Host the application on the ec2 instance
   }
 }
